12f5e6e8ea84a84072083898239c7f7f303a548a
Charlie Zhang <cz317@georgetown.edu>
2024-07-10 16:04:03 -0400

    Misc UI improvemnts

--- src/renderer/src/pages/About.jsx
+++ src/renderer/src/pages/About.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
+import fastrLogo from '../assets/FASTR_Logo_White_En.png'
 
-function Versions() {
+const Versions = () => {
   const [versions] = useState(window.electron.process.versions)
 
   return (
-    <ul className="versions">
-      <li className="electron-version">Electron v{versions.electron}</li>
-      <li className="chrome-version">Chromium v{versions.chrome}</li>
-      <li className="node-version">Node v{versions.node}</li>
-    </ul>
+    <>
+      <ul className="versions">
+        <li className="electron-version">Electron v{versions.electron}</li>
+        <li className="chrome-version">Chromium v{versions.chrome}</li>
+        <li className="node-version">Node v{versions.node}</li>
+      </ul>
+      <div className="relative w-10 h-10 md:w-16 md:h-16">
+        <img src={fastrLogo} className="rounded-lg" />
+      </div>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/renderer/index.html', './src/renderer/src/**/*.{svelte,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'teal-green': '#2DFFDC',
+        'select-background': '#fff'
+      }
+    }
   },
   plugins: []
 }
